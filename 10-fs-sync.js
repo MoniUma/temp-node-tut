cbcb5a8a5ccdb353b982ac5390c3dc89915f7968
Tura <Chikunala@protonmail.com>
2024-01-22 17:33:58 +0800

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,17 @@
+const {readFileSync, writeFileSync} = require('fs')
+console.log('start');
+
+// syntax 2:
+// const fs = require('fs')
+// fs.readFileSync
+// fs.writeFileSync
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+writeFileSync('./content/result-sync.txt', 
+    `Here is the result : ${first}, ${second}`, 
+    {flag: 'a'}
+)
+console.log('done with this task');
+console.log('starting next one');
